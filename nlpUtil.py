e2c03cc2bf08b0d550ad4c76229cd6a6c73c44c0
Anwesh43 <anweshthecool0@gmail.com>
2017-01-04 15:02:37 +0530

    using . plus space instead of only .

--- nlpUtil.py
+++ nlpUtil.py
@@ -1,7 +1,6 @@
 def tokenize_sentence(sentence):
     return sentence.split(' ')
 def tokenize_paragraph(paragraph):
-    sentences =  paragraph.split('.')
-    if sentences[len(sentences)-1].rstrip() == "":
-        setences = sentences[:-1]
-    return setences
+    def replace_dot(sentence):
+        return sentence.replace('.','')
+    return map(replace_dot,paragraph.split('. '))
